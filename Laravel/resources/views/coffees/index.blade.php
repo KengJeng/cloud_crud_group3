0265c999dcc8680ebfcdeef1d1a035659e61caa6
Legaspi_John Wayne <johnwayne.d.legaspi@isu.edu.ph>
2026-02-08 12:18:42 +0000

    add coffee list page

---
+++ Laravel/resources/views/coffees/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Coffee List</title>
+</head>
+<body>
+
+<h1>Coffee Menu</h1>
+
+<table border="1" cellpadding="10">
+    <tr>
+        <th>ID</th>
+        <th>Coffee Name</th>
+        <th>Price</th>
+    </tr>
+
+    <tr>
+        <td>1</td>
+        <td>Americano</td>
+        <td>₱120</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Latte</td>
+        <td>₱150</td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Cappuccino</td>
+        <td>₱140</td>
+    </tr>
+    <tr>
+        <td>4</td>
+        <td>Espresso</td>
+        <td>₱100</td>
+    </tr>
+    <tr>
+        <td>5</td>
+        <td>Mocha</td>
+        <td>₱160</td>
+    </tr>
+
+</table>
+
+</body>
+</html>
--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -6,4 +6,6 @@ Route::get('/', function () {
     return view('test');
 });
 
-Route::apiResource('coffees', App\Http\Controllers\CoffeeController::class);
+Route::get('/coffees', function () {
+    return view('coffees.index');
+});
