4a104eb96a0122b165a589cc4a77ac58389bc481
Legaspi_John Wayne <johnwayne.d.legaspi@isu.edu.ph>
2026-02-09 15:58:12 +0000

    Add coffee list view with delete button

--- Laravel/resources/views/coffees/index.blade.php
+++ Laravel/resources/views/coffees/index.blade.php
@@ -5,49 +5,58 @@
 </head>
 <body>
 
-<h1>☕ Coffee Menu</h1>
-
-
-<p>
-    <a href="{{ route('coffees.create') }}">
-        <button>Add Coffee ☕</button>
-    </a>
-</p>
-
-
-@if (session('success'))
-    <p style="color: green;">{{ session('success') }}</p>
-@endif
+<h1>Coffee Menu</h1>
 
 <table border="1" cellpadding="10">
     <tr>
-        <th>Image</th>
+        <th>ID</th>
         <th>Name</th>
         <th>Category</th>
         <th>Price</th>
+        <th>Action</th>
     </tr>
 
     @foreach ($coffees as $coffee)
     <tr>
-        <td>
-          
-            @if ($coffee->image_path)
-                <img src="{{ asset($coffee->image_path) }}" width="100">
-            @else
-                No image
-            @endif
-        </td>
-        <td>
-            <a href="{{ route('coffees.show', $coffee->id) }}">
-                {{ $coffee->name }}
-            </a>
-        </td>
+        <td>{{ $coffee->id }}</td>
+        <td>{{ $coffee->name }}</td>
         <td>{{ $coffee->category }}</td>
         <td>₱{{ number_format($coffee->price, 2) }}</td>
+        <td>
+            <button onclick="deleteCoffee({{ $coffee->id }})">
+                Delete
+            </button>
+        </td>
     </tr>
     @endforeach
 
 </table>
+
+<script>
+// Call CoffeeController
+function deleteCoffee(id) {
+    if (!confirm('Are you sure you want to delete this coffee?')) {
+        return;
+    }
+
+    fetch(`/coffees/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'X-CSRF-TOKEN': '{{ csrf_token() }}',
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+        location.reload(); // refresh list
+    })
+    .catch(error => {
+        console.error(error);
+        alert('Failed to delete coffee');
+    });
+}
+</script>
 
 </body>
 </html>
--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -3,17 +3,29 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CoffeeController;
 
+
+
+// Redirect home to coffee list
 Route::get('/', function () {
-    return view('coffees.index');
+    return redirect()->route('coffees.index');
 });
 
+// Show coffee list (view)
+Route::get('/coffees', [CoffeeController::class, 'index'])
+    ->name('coffees.index');
 
-Route::get('/coffees', [CoffeeController::class, 'index'])->name('coffees.index');
+// Create coffee form
+Route::get('/coffees/create', [CoffeeController::class, 'create'])
+    ->name('coffees.create');
 
-Route::get('/coffees/create', [CoffeeController::class, 'create'])->name('coffees.create');
+// Store coffee
+Route::post('/coffees', [CoffeeController::class, 'store'])
+    ->name('coffees.store');
 
+// Show single coffee
+Route::get('/coffees/{coffee}', [CoffeeController::class, 'show'])
+    ->name('coffees.show');
 
-Route::post('/coffees', [CoffeeController::class, 'store'])->name('coffees.store');
-
-
-Route::get('/coffees/{coffee}', [CoffeeController::class, 'show'])->name('coffees.show');
+// Delete coffee
+Route::delete('/coffees/{coffee}', [CoffeeController::class, 'destroy'])
+    ->name('coffees.destroy');
